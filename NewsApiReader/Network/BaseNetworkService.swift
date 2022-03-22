cb66504ebfd77c360f66b501f0498302e900d862
Ihar Biarozkin <biarozkin@gmail.com>
2022-03-22 16:50:09 +0300

    change BaseNetworkService to use JSONParamsEncoder or URLParamsEncoder while configuring parameters

--- NewsApiReader/Network/BaseNetworkService.swift
+++ NewsApiReader/Network/BaseNetworkService.swift
@@ -7,11 +7,6 @@
 
 import Foundation
 
-enum EndpointPath: String {
-    case allSources = "v2/top-headlines/sources"
-    case bySource = "/v2/everything"
-}
-
 enum RequestResult {
     case failure(error: String)
     case value(data: Data, code: Int)
@@ -30,11 +25,11 @@ class BaseNetworkService {
         self.token = "143799e303f24d9690db184e40f94baf" // FIXME: - Save token using keychain
     }
     
-    func sendRequest(endPoint: EndpointPath,
+    func sendRequest(endPoint: EndPointType,
                      completion: @escaping (RequestResult) -> Void) {
         // FYI. URLRequest was used to save token in a secure way into HTTP header.
         // https://newsapi.org/docs/authentication
-        guard let urlRequest = makeURLRequest(endPointPath: endPoint) else {
+        guard let urlRequest = makeRequestFor(endpoint: endPoint) else {
             return
         }
         
@@ -60,12 +55,20 @@ class BaseNetworkService {
         }
         task.resume()
     }
-
-    private func makeURLRequest(endPointPath: EndpointPath) -> URLRequest? {
-        guard let url = buildURL(baseURLString: baseURLString,
-                                 endpointPath: endPointPath.rawValue) else { return nil }
-        let withToken = addToken(url: url)
-        return withToken
+    
+    func makeRequestFor(endpoint: EndPointType) -> URLRequest? {
+        var request: URLRequest?
+        switch endpoint.parameterEncoding {
+        case .json:
+            request = JSONParamsEncoder.encode(baseURLString: baseURLString,
+                                               endpoint: endpoint,
+                                               token: token)
+        case .url:
+            request = URLParamsEncoder.encode(baseURLString: baseURLString,
+                                              endpoint: endpoint,
+                                              token: token)
+        }
+        return request
     }
     
     private func addToken(url: URL) -> URLRequest? {
@@ -73,11 +76,6 @@ class BaseNetworkService {
         // FIXME: - Use keychain to save token securely.
         request.allHTTPHeaderFields = [HeaderKeys.token.rawValue: token]
         return request
-    }
-    
-    private func buildURL(baseURLString: String,
-                          endpointPath: String) -> URL? {
-        return URL(string: baseURLString + endpointPath)
     }
 }
 
