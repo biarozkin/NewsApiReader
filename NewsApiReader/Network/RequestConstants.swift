ca73f1dbbc04cb09b61f2f82913b4208fac97d5f
Ihar Biarozkin <biarozkin@gmail.com>
2022-03-22 16:51:11 +0300

    add RequestsConstants to use with RequestsEndpoint

--- NewsApiReader/Network/RequestConstants.swift
+++ NewsApiReader/Network/RequestConstants.swift
@@ -8,10 +8,37 @@
 import Foundation
 
 enum HeaderKeys: String {
+    case contentType = "Content-Type"
     case token = "Authorization"
 }
 
 enum NetworkCodes: Int {
     case success = 200
     // TODO: - Describe other situations.
+}
+
+enum HTTPMethod: String {
+    case post = "POST"
+    case get = "GET"
+    case put = "PUT"
+    case delete = "DELETE"
+}
+
+protocol EndPointType: BaseEndPointType {
+    var params: [String: Any]? { get }
+    var parameterEncoding: ParameterEncoding { get }
+}
+
+protocol BaseEndPointType {
+    var baseHeaders: [String: String] { get }
+    
+    var path: String { get }
+    var method: HTTPMethod { get }
+    var headers: [String: String]? { get }
+}
+
+extension BaseEndPointType {
+    var baseHeaders: [String: String] {
+        return [HeaderKeys.contentType.rawValue: "application/json"]
+    }
 }
